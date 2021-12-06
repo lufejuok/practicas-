21fff42e1cbb9b5794e37d1c4eea01ee0c9e0d36
Luisa Fernanda Jurado Ocampo <luisa@MacBook-Air-de-Luisa.local>
2021-12-06 13:08:05 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let resultado=document.querySelector('#numero');
+let a=0;
+
+function Sumar(){
+    a=a+1;
+     resultado.innerHTML=a;
+}
+
+
+let resultado2=document.querySelector('#numero2');
+let b=0;
+
+function Sumar2(){
+    b=b+1;
+     resultado2.innerHTML=b;
+}
